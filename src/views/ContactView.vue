e9c83bc83ad41b0fd405fa6f7f1e5a504956dd35
Sebastian <mail@sebastian-kendra.sk>
2022-07-30 23:40:24 +0200

    tvorba kontaktu

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an contact</h1>
+    <form action="#" class="contact-form">
+      <label for="your-name">
+        Vaše meno (povinne)
+        <input type="text" id="your-name" name="your-name" />
+      </label>
+
+      <label for="your-email">
+        Váš email (povinne)
+        <input type="email" id="your-email" name="your-email" />
+      </label>
+
+      <label for="your-message">
+        Vaša správa
+        <textarea
+          id="your-message"
+          name="your-message"
+          cols="40"
+          rows="10"
+          style="width: 600px; height: 183px"
+        ></textarea>
+      </label>
+
+      <button class="btn second-btn"><p>Send</p></button>
+    </form>
   </div>
 </template>
