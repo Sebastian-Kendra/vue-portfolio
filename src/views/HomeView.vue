57b3ff5128e40307d8dc56f45ee11822b6372212
Sebastian <mail@sebastian-kendra.sk>
2022-08-03 23:37:17 +0200

    neviem kaamo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="home-view">
-    <div class="flex">
+    <div class="flex" :class="{ active: fadeIn }">
       <HomeHero />
       <LogoSvg />
     </div>
-
+    <div class="about-me">
+      <hr />
+      <p>
+        Niečo o mne Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Saepe, eveniet. Debitis impedit pariatur earum quos, cupiditate dicta.
+        Excepturi, nulla enim dolore, commodi in modi distinctio nemo neque,
+        pariatur aspernatur facere!
+      </p>
+    </div>
     <div class="fade-in-footer">
       <FooterView />
     </div>
@@ -15,9 +23,29 @@
 import LogoSvg from '@/components/LogoSvg.vue'
 import FooterView from '@/components/FooterView.vue'
 import HomeHero from '../components/HomeHero.vue'
+import { reactive, toRefs } from 'vue'
 
 export default {
   name: 'HomeView',
+  setup() {
+    const state = reactive({
+      isActive: false,
+    })
+
+    const resizeHand = (e) => {
+      console.log(e)
+    }
+
+    setTimeout(() => {
+      state.isActive = true
+    }, '200')
+
+    return {
+      ...toRefs(state),
+      resizeHand,
+    }
+  },
+
   components: { FooterView, LogoSvg, HomeHero },
 }
 </script>
