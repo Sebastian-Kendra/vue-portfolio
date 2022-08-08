4fa880c4b6d98ca6a7b3521afdb54ec667b2084a
Sebastian <mail@sebastian-kendra.sk>
2022-08-08 22:42:57 +0200

    disable css

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="navigation">
     <router-link to="/"> <span> O mne </span></router-link>
-    <router-link to="/projects"> <span> Projekty </span> </router-link>
+    <router-link to="/projects" title="disabled" class="disabled">
+      <span> Projekty </span>
+    </router-link>
     <router-link to="/contact"> <span> Kontakt </span> </router-link>
   </div>
 </template>
