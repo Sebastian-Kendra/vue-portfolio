b9863ada6820a3c26d082dc04feb5c273badaa23
Sebastian <mail@sebastian-kendra>
2022-08-13 09:08:20 +0200

    cursor build

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="cursor"></div>
   <div id="app">
     <header class="fade-in">
       <TheNavigation />
@@ -16,6 +17,44 @@ import TheNavigation from './components/TheNavigation.vue'
 export default {
   components: {
     TheNavigation,
+  },
+  mounted() {
+    this.cursor()
+  },
+  methods: {
+    cursor() {
+      /*  eslint-disable no-undef */
+      /* eslint-disable no-unused-vars  */
+      const updateProperties = (elem, state) => {
+        elem.style.setProperty('--x', `${state.x}px`)
+        elem.style.setProperty('--y', `${state.y}px`)
+        elem.style.setProperty('--width', `${state.width}px`)
+        elem.style.setProperty('--height', `${state.height}px`)
+        elem.style.setProperty('--radius', state.radius)
+        elem.style.setProperty('--scale', state.scale)
+      }
+
+      document.querySelectorAll('.cursor').forEach((cursor) => {
+        const createState = (e) => {
+          const defaultState = {
+            x: e.clientX,
+            y: e.clientY,
+            width: 42,
+            height: 42,
+            radius: '100px',
+          }
+
+          return {
+            ...defaultState,
+          }
+        }
+
+        document.addEventListener('mousemove', (e) => {
+          const state = createState(e)
+          updateProperties(cursor, state)
+        })
+      })
+    },
   },
 }
 </script>
