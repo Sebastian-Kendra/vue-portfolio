6113c5aa69201c206f995b2679ae678ef8c878f0
Sebastian <mail@sebastian-kendra>
2022-09-23 19:51:36 +0200

    prvotne vytvorenie pod. projekty

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="project-view"></div>
+  <div class="project-view">
+    <!-- 
+    <div class="parallax">
+      <div class="parallax__layer parallax__layer--back">...</div>
+      <div class="parallax__layer parallax__layer--base">...</div> -->
+
+    <article class="project">
+      <figure class="img-con">
+        <a href=""><img src="" alt="" /></a>
+      </figure>
+      <div class="purport">
+        <h2>Organizačny List</h2>
+        <h3>Vytvor si list a kartu</h3>
+        <p>
+          Pri tvorbe organizačneho listu bol použity framework Vue 3. Vytvorene
+          komponenty boli pisane spôsobom composition API. Použity jazyk
+          javascript.
+        </p>
+      </div>
+    </article>
+
+    <article class="project">
+      <figure class="img-con">
+        <a href=""><img src="" alt="" /></a>
+      </figure>
+      <div class="purport">
+        <h2></h2>
+        <h3></h3>
+        <p></p>
+      </div>
+    </article>
+
+    <article class="project">
+      <figure class="img-con">
+        <a href=""><img src="" alt="" /></a>
+      </figure>
+      <div class="purport">
+        <h2></h2>
+        <h3></h3>
+        <p></p>
+      </div>
+    </article>
+  </div>
+  <!--   </div> -->
 </template>
 
 <script>
